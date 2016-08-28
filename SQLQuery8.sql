7bfadcba0b6369bc43a32fee50d838ad4be77746
David Cramb <david.cramb@gmail.com>
2016-08-27 22:35:53 -0500

    8

---
+++ SQLQuery8.sql
@@ -0,0 +1,2 @@
+﻿SELECT Invoice.Total, CONCAT(Customer.FirstName, ' ',Customer.LastName) AS CustomerName, Customer.Country, CONCAT(Employee.FirstName, ' ', Employee.LastName) AS Employee FROM Invoice JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee ON EmployeeId = Customer.SupportRepId;
