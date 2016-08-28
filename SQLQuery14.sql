d3475e8b1deced35149d652a0b43e3bf1779a030
David Cramb <david.cramb@gmail.com>
2016-08-28 15:47:31 -0500

    14

---
+++ SQLQuery14.sql
@@ -0,0 +1,2 @@
+﻿--Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT BillingCountry, COUNT(BillingCountry) FROM Invoice GROUP BY BillingCountry;
