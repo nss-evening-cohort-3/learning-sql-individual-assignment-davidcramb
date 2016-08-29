9b21734e3075c2d460758cb78eae5de7519d3d7a
David Cramb <david.cramb@gmail.com>
2016-08-29 17:02:43 -0500

    16 & 17

---
+++ SQLQuery16.sql
@@ -0,0 +1,7 @@
+﻿--Provide a query that shows all the Tracks, but displays no IDs. 
+--The resultant table should include the Album name, Media type and Genre.
+SELECT Album.Title AS Album, Track.Name AS Track, MediaType.Name AS Media, Genre.Name AS Genre FROM Track
+JOIN Album ON Album.AlbumId = Track.AlbumId
+JOIN MediaType ON MediaType.MediaTypeId = Track.MediaTypeId
+JOIN Genre ON Genre.GenreId = Track.GenreId
+ORDER BY Genre ASC;
---
+++ SQLQuery17.sql
@@ -0,0 +1,5 @@
+﻿--Provide a query that shows all Invoices but includes the # of invoice line items.
+SELECT Invoice.InvoiceId, Invoice.CustomerId, Invoice.InvoiceDate, Invoice.Total, COUNT(InvoiceLine.InvoiceId) AS LineItems FROM Invoice
+JOIN InvoiceLine ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+GROUP BY Invoice.InvoiceId, Invoice.CustomerId, Invoice.InvoiceDate, Invoice.Total
+ORDER BY InvoiceId ASC;
