0c752a2644e97524d6d30d63e6c2722a896f42a6
David Cramb <david.cramb@gmail.com>
2016-08-28 20:37:17 -0500

    15

---
+++ SQLQuery15.sql
@@ -0,0 +1,5 @@
+﻿--Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.
+
+SELECT Name, COUNT(PlaylistTrack.TrackId) FROM Playlist
+JOIN PlaylistTrack ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+GROUP BY Name;
