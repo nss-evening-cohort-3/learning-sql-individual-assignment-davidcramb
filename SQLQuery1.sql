a5b0e594323c5f8c2c754c2e798bf95a2fb14b2d
David Cramb <david.cramb@gmail.com>
2016-08-29 18:30:40 -0500

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,4 @@
+﻿--SQL Version
 ﻿SELECT CONCAT (FirstName, ' ', LastName) FROM Customer WHERE Customer.Country != 'USA';
+﻿--SQLite Version
+﻿SELECT FirstName || ' ' || LastName AS Customer FROM Customer WHERE Customer.Country != 'USA';
