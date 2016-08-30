7f90572a281284944277bd597a4b1e6bfa9d6b27
David Cramb <david.cramb@gmail.com>
2016-08-29 20:35:00 -0500

    19 revised

--- SQLQuery19.sql
+++ SQLQuery19.sql
@@ -1,7 +1,5 @@
-﻿--Which sales agent made the most in sales in 2009? HINT: MAX
---This works in SQLite Manager ONLY
-SELECT Employee.LastName, SUM(Invoice.Total) FROM Employee 
+﻿SELECT LastName, MAX(Revenue) FROM(SELECT Employee.LastName, SUM(Invoice.Total) AS Revenue FROM Employee 
 JOIN Customer ON Customer.SupportRepId = Employee.EmployeeId
 JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
 WHERE InvoiceDate LIKE '2009%'
-GROUP BY Employee.LastName;
+GROUP BY Employee.LastName)
