a261e0cec52288f515ee31c0142f33b68eff43ac
David Cramb <david.cramb@gmail.com>
2016-08-29 21:02:27 -0500

    22

---
+++ SQLQuery22.sql
@@ -0,0 +1,4 @@
+﻿--SQLite Version
+SELECT Employee.FirstName || ' ' || Employee.LastName AS Employee, COUNT(Customer.SupportRepId) AS Total_Customers FROM Employee
+JOIN Customer ON Customer.SupportRepId == EmployeeId
+GROUP BY Employee
