88892c10016f97eb3a9037ef4632e93b1bb20e9b
David Cramb <david.cramb@gmail.com>
2016-08-29 20:20:39 -0500

    18, 20

---
+++ SQLQuery18.sql
@@ -0,0 +1,8 @@
+﻿--Provide a query that shows total sales made by each sales agent.
+SELECT Employee.EmployeeId, CONCAT(Employee.FirstName, ' ', Employee.LastName) AS 'Employee Name',
+COUNT(Customer.SupportRepId) AS 'Total Sales',
+SUM(Invoice.Total) AS 'Revenue'
+FROM Employee
+JOIN Customer ON SupportRepId = Employee.EmployeeId
+JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+GROUP BY Employee.EmployeeId, Employee.FirstName, Employee.LastName;
---
+++ SQLQuery20.sql
@@ -0,0 +1,7 @@
+﻿--Which sales agent made the most in sales in 2009? HINT: MAX
+--This works in SQLite Manager ONLY
+SELECT Employee.LastName, SUM(Invoice.Total) FROM Employee 
+JOIN Customer ON Customer.SupportRepId = Employee.EmployeeId
+JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+WHERE InvoiceDate LIKE '2009%'
+GROUP BY Employee.LastName;
