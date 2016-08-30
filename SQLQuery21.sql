f6e3e6ff20f72f24649a6e514e8a4520e10cc4ec
David Cramb <david.cramb@gmail.com>
2016-08-29 20:39:23 -0500

    21

---
+++ SQLQuery21.sql
@@ -0,0 +1,4 @@
+﻿SELECT LastName, MAX(Revenue) FROM(SELECT Employee.LastName, SUM(Invoice.Total) AS Revenue FROM Employee 
+JOIN Customer ON Customer.SupportRepId = Employee.EmployeeId
+JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+GROUP BY Employee.LastName)
