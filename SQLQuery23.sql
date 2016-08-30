0584608ebe2c319fa9e945fd7d90040d4282e9ca
David Cramb <david.cramb@gmail.com>
2016-08-29 21:27:20 -0500

    23

---
+++ SQLQuery23.sql
@@ -0,0 +1,6 @@
+﻿--Provide a query that shows the total sales per country. Which country's customers spent the most?
+SELECT DISTINCT(BillingCountry) AS Country, COUNT(BillingCountry) AS Total_Sales FROM Invoice
+GROUP BY Country
+--
+SELECT Country, MAX(Total_Sales) FROM (SELECT DISTINCT(BillingCountry) AS Country, COUNT(BillingCountry) AS Total_Sales FROM Invoice
+GROUP BY Country)
