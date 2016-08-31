3d060f6282653acd9d1358690f03dcbb4f4e3e5c
David Cramb <david.cramb@gmail.com>
2016-08-30 21:46:39 -0500

    24

---
+++ SQLQuery24.sql
@@ -0,0 +1,8 @@
+﻿--SQLITE
+--Provide a query that shows the most purchased track of 2013.
+SELECT TrackId, Name, Downloads FROM (SELECT DISTINCT(InvoiceLine.TrackId), Invoice.InvoiceDate, Track.Name, COUNT(InvoiceLine.TrackId) AS 'Downloads' FROM InvoiceLine
+JOIN Track ON Track.TrackId == InvoiceLine.TrackId
+JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+GROUP BY InvoiceLine.TrackId, Track.Name
+ORDER BY Downloads DESC)
+WHERE strftime('%Y', InvoiceDate) == '2013'
