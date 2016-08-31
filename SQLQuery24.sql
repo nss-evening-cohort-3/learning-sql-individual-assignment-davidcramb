054c026fa55686aa252d11437eced8c2d351a4cb
David Cramb <david.cramb@gmail.com>
2016-08-31 18:06:05 -0500

    fixing 24 again

--- SQLQuery24.sql
+++ SQLQuery24.sql
@@ -1,9 +1,9 @@
 ﻿--SQLITE
---Provide a query that shows the most purchased track of 2013.
-SELECT InvoiceLine.TrackId, Track.Name, COUNT(InvoiceLine.TrackId) AS 'Downloads' FROM InvoiceLine
-JOIN Track ON Track.TrackId == InvoiceLine.TrackId JOIN Invoice ON Invoice.InvoiceId = InvoiceLine.InvoiceId
-WHERE strftime('%Y',Invoice.InvoiceDate) == '2013' 
-GROUP BY Track.Name
-ORDER BY Downloads DESC
-LIMIT 1
-
+--Provide a query that shows the top 5 most purchased tracks over all.
+SELECT TrackId, Name, Downloads FROM (SELECT DISTINCT(InvoiceLine.TrackId), Invoice.InvoiceDate, Track.Name, COUNT(InvoiceLine.TrackId) AS 'Downloads' FROM InvoiceLine
+JOIN Track ON Track.TrackId == InvoiceLine.TrackId
+JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+GROUP BY InvoiceLine.TrackId, Track.Name
+ORDER BY Downloads DESC)
+WHERE strftime('%Y', InvoiceDate) == '2013'
+LIMIT 1;
