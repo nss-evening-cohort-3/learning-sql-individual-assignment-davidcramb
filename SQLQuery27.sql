23f866361b85a442aa0c4b00efe2133d8e97ea68
David Cramb <david.cramb@gmail.com>
2016-08-31 18:43:50 -0500

    27

---
+++ SQLQuery27.sql
@@ -0,0 +1,6 @@
+﻿SELECT Media_Type, COUNT(Media_Type) AS Total_Purchased FROM (SELECT InvoiceLine.TrackId, Track.Name, Track.MediaTypeId, MediaType.Name AS Media_Type FROM InvoiceLine
+JOIN Track ON Track.TrackId == InvoiceLine.TrackId
+JOIN MediaType ON MediaType.MediaTypeId == Track.MediaTypeId)
+GROUP BY Media_Type
+ORDER BY Total_Purchased DESC
+
