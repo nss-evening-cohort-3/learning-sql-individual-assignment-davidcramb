c0454e74b91a2d47c891d24a2dc0077a0b62ca41
David Cramb <david.cramb@gmail.com>
2016-08-31 18:07:45 -0500

    25

---
+++ SQLQuery25.sql
@@ -0,0 +1,8 @@
+﻿--SQLITE
+--Provide a query that shows the top 5 most purchased tracks over all.
+SELECT TrackId, Name, Downloads FROM (SELECT DISTINCT(InvoiceLine.TrackId), Invoice.InvoiceDate, Track.Name, COUNT(InvoiceLine.TrackId) AS 'Downloads' FROM InvoiceLine
+JOIN Track ON Track.TrackId == InvoiceLine.TrackId
+JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+GROUP BY InvoiceLine.TrackId, Track.Name
+ORDER BY Downloads DESC)
+LIMIT 5;
